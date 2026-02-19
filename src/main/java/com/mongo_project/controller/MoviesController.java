76b7e79f1da7721c7d27e0c5e513d018c0bba1e5
mohit12141 <mohitsahuimp@gmail.com>
2026-02-20 00:11:33 +0530

    Added Request Param in MoviesController

--- src/main/java/com/mongo_project/controller/MoviesController.java
+++ src/main/java/com/mongo_project/controller/MoviesController.java
@@ -1,17 +1,17 @@
 package com.mongo_project.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.mongo_project.entity.Movies;
-import com.mongo_project.service.MovieService;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mongo_project.entity.Movies;
+import com.mongo_project.service.MovieService;
 
 
 @RestController
@@ -21,13 +21,13 @@ public class MoviesController {
     private MovieService movieService;
 
     @GetMapping("")
-    public ResponseEntity<List<Movies>> getAllMovies() {
-        List<Movies> list = movieService.getAllMovies(10, 10);
+    public ResponseEntity<List<Movies>> getAllMovies(@RequestParam(defaultValue = "10") int page, @RequestParam(defaultValue = "10") int size) {
+        List<Movies> list = movieService.getAllMovies(page, size);
         if(list.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
 
         return ResponseEntity.ok(list);
     }
-    
+
 }
